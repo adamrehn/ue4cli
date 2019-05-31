1c6e6d0a31d8b0d44adf539010ee12948ae94ac6
Gabriel Candal <gcc@platforme.com>
2019-05-31 17:52:27 +0100

    Add __main__

---
+++ ue4cli/__main__.py
@@ -0,0 +1,9 @@
+from .cli import main
+import os, sys
+
+if __name__ == '__main__':
+
+    # Rewrite sys.argv[0] so our help prompts display the correct base command
+    interpreter = sys.executable if sys.executable not in [None, ''] else 'python3'
+    sys.argv[0] = '{} -m ue4cli'.format(os.path.basename(interpreter))
+    main()
