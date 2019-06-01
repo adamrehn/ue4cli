73d7652524b263e42f456c6cc9f7a699df56286d
Adam Rehn <adam.rehn@my.jcu.edu.au>
2019-06-01 12:56:36 +1000

    Make indentation in __main__.py consistent with other files

--- ue4cli/__main__.py
+++ ue4cli/__main__.py
@@ -2,8 +2,8 @@ from .cli import main
 import os, sys
 
 if __name__ == '__main__':
-
-    # Rewrite sys.argv[0] so our help prompts display the correct base command
-    interpreter = sys.executable if sys.executable not in [None, ''] else 'python3'
-    sys.argv[0] = '{} -m ue4cli'.format(os.path.basename(interpreter))
-    main()
+	
+	# Rewrite sys.argv[0] so our help prompts display the correct base command
+	interpreter = sys.executable if sys.executable not in [None, ''] else 'python3'
+	sys.argv[0] = '{} -m ue4cli'.format(os.path.basename(interpreter))
+	main()
